068e8ae1b86f87a3270d32708d267ec2a5b41ba4
eincherjar <marcinbarszcz2@gmail.com>
2026-01-25 20:20:55 +0100

    Fix import in templatetags/__init__.py - use smart_translate instead of auto_translate

--- src/djcrudx/templatetags/__init__.py
+++ src/djcrudx/templatetags/__init__.py
@@ -1,15 +1,15 @@
 from django import template
-from ..translations import auto_translate
+from ..translations import smart_translate
 
 register = template.Library()
 
 @register.simple_tag
 def trans(text):
     """Automatyczne tłumaczenie bez potrzeby makemessages"""
-    return auto_translate(text)
+    return smart_translate(text)
 
 @register.simple_tag  
 def blocktrans(text, **kwargs):
     """Automatyczne tłumaczenie z parametrami"""
-    translated = auto_translate(text)
+    translated = smart_translate(text)
     return translated.format(**kwargs)
