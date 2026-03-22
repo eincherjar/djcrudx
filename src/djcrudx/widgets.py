984d0dc9438f20d3673d1d8133aac9debc4799fb
eincherjar <marcinbarszcz2@gmail.com>
2026-03-22 11:40:29 +0100

    add SecretInputWidget

--- src/djcrudx/widgets.py
+++ src/djcrudx/widgets.py
@@ -778,3 +778,38 @@ class TextareaWidget(forms.Textarea):
         if attrs:
             default_attrs.update(attrs)
         super().__init__(attrs=default_attrs)
+
+
+class SecretInputWidget(Widget):
+    """Widget dla pola hasła z przyciskiem toggle show/hide"""
+
+    def render(self, name, value, attrs=None, renderer=None):
+        if attrs is None:
+            attrs = {}
+
+        ui_colors = get_ui_colors()
+        field_id = attrs.get("id", f"id_{name}")
+        input_classes = f"w-full px-3 py-1 text-xs bg-white border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-{ui_colors['primary_ring']}"
+        display_value = value if value else ""
+
+        html = f'''
+            <div class="relative" x-data="{{ show: false }}">
+                <input :type="show ? 'text' : 'password'" name="{name}" id="{field_id}" value="{display_value}"
+                       class="{input_classes} pr-8">
+                <button type="button" @click="show = !show"
+                        class="absolute right-2 top-1/2 -translate-y-1/2 text-gray-400 hover:text-gray-600">
+                    <svg x-show="!show" class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                              d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path>
+                    </svg>
+                    <svg x-show="show" class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                              d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.542-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.542 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"></path>
+                    </svg>
+                </button>
+            </div>
+        '''
+
+        return mark_safe(html)
